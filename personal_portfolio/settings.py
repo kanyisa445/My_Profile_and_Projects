fd39c993eb4251ca2d6eb3dc43bd9a36e70930f4
Kanyisa <ddfhpc@gmail.com>
2021-06-07 14:00:10 +0200

    tweaked app

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mwwf&^d79enz9a$aq12c9xfq&uz8&295=-15l7_k$0iv0m#=b('
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
