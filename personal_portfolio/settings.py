6273ab03a10f833f3ba4d041f146c15093de544c
Kanyisa <ddfhpc@gmail.com>
2021-06-17 17:13:52 +0200

    .

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "i=)+5-*dw8y@$pt!v7ncf1lo1kmgj=0o+o8w03_ifxm9&3vduo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
