953ee3b289d58a2f9ac4c1fbb9a0bd83caaaa8c1
Kanyisa <ddfhpc@gmail.com>
2021-06-17 16:31:06 +0200

    tweeked for production

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,8 +127,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
